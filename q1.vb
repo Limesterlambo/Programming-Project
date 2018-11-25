f4fdb9ce600235061abf2506a48f634c87635f84
Nickq20 <45201207+Nickq20@users.noreply.github.com>
2018-11-25 12:42:12 +0000

    24/11/18 (#1)

--- q1.vb
+++ q1.vb
@@ -1,18 +1,32 @@
 ﻿Module Module1
     Function Table_Headings() 'no inputs required'
         'making the headings of the table'
-        Console.WriteLine("Material /" & vbTab & "Depth (meters) /" & vbTab & "Average Speed (m/s) /" & vbTab & "Time Spent in the medium (s)")
-        Console.WriteLine("---------------------------------------------------------------------------------------------")
+        Console.WriteLine("Material /" & vbTab & "Time Taken (s)")
+        Console.WriteLine("---------------------------------")
+    End Function
+
+    Function Distance_In(Wood As Double, Steel As Double, Titanium As Double) 'assighning our array of depths to internal variables within the function'
+        Dim Output
+        'making a variable within the function = what we want it to output, so we state what we want our internal function variables to = and then get them to = the format we want to output'
+        Output = "For distance in wood you entered: " & Wood & vbNewLine & "For distance in steel you entered: " & Steel & vbNewLine & "For distance in titanium you entered: " & Titanium
+        Console.WriteLine(Output)
+    End Function
+
+    Function average_time_to_reach_targer(Wood As Integer, Steel As Integer, Titanium As Integer, WoodD As Integer, SteelD As Integer, TitaniumD As Integer) 'Wood being the average speed the bullet travels through wood and woodD being the distance the bullet was fired from the wood'
+        Console.WriteLine("On average it took " & ((WoodD / Wood) + (SteelD / Steel) + (SteelD / Titanium)) / 3 & " seconds for the bullet to reach the targer")
     End Function
 
     Sub Main()
+
         'Pre given values placed in acording arrays'
         Dim average_speeds() As Double = {323.5, 180.8, 100.3}
+        Dim Distance_from_material() As Integer = {800, 500, 250}
         Dim material() As String = {"Wood    ", "Steel   ", "Titanium"}
         'A variable for the user to input a depth for ecah material'
-        Dim Depth(2) As Double
+        Dim Depth(2)
         'a usfull index for out for loops to work'
         Dim i, i1
+
 
         'asking the user to input a depth and then assighning that value to a possition in an array'
         For i = 0 To 2
@@ -29,14 +43,23 @@
             Next
         Next
 
+        Console.WriteLine(vbNewLine)
+
+        Distance_In(Depth(0), Depth(1), Depth(2))
+
+        Console.WriteLine(vbNewLine)
+
+        average_time_to_reach_targer(average_speeds(0), average_speeds(1), average_speeds(2), Distance_from_material(0), Distance_from_material(1), Distance_from_material(2))
+
+        Console.WriteLine(vbNewLine)
+
         'outputing the table of results'
         Table_Headings()
 
         'outputing the rows'
         For i = 0 To 2
-            Console.WriteLine(material(i) & " /" & vbTab & Space(3) & Format(Depth(i), "00.000") & Space(6) & "/" & Space(10) & Format(average_speeds(i), "0.000") & Space(11) & "/" & Space(11) & Format(Depth(i) / average_speeds(i), "0.000"))
+            Console.WriteLine(material(i) & " /" & vbTab & Format(Depth(i) / average_speeds(i), "0.000"))
         Next
 
     End Sub
-
 End Module
