85e9c28b3d8cd8d6bc17508153db6b01573be1bd
Nickq20 <45201207+Nickq20@users.noreply.github.com>
2018-12-07 16:45:39 +0000

    Add files via upload (#6)

--- q1.vb
+++ q1.vb
@@ -1,19 +1,14 @@
-'07/12/18.
-'Nicholas Letchford-Jones.
-'This program is desighend to be helpfull to the scientists who will be using it, asking them to input depths, checking wether the value they inputed was a valed one, aka above 100; and then going throw a secondary verification process of confirming with them the value they inpute. the program will then inform the scientists the period the buttel spent in the medium, and then the average times over the 3 materials the bullet spent in the medium .
-
-Module Module1
-    Dim t
+﻿Module Module1
     Dim current_line
 
-    'to seperate your work out in the command prompt.
+    '  To seperate your work out in the command prompt.
     Function newlines(Number_of_newlines)
         For i = 1 To Number_of_newlines
             Console.Write(vbNewLine)
         Next
     End Function
 
-    'General up to 5 column table maker.
+    '  General up to 5 column table maker.
     Function Table_Heading(Number_of_columnns As Integer, Heading1 As String, Heading2 As String, Heading3 As String, Heading4 As String, Heading5 As String)
         Dim n
         n = Number_of_columnns
@@ -46,29 +41,27 @@ Module Module1
         Else
             Console.WriteLine("")
         End If
-        'If the table devision is off, modify the number of times "-" string is duplicated.
+        ' If the table devision is off, modify the number of times "-" string is duplicated.
         Console.WriteLine(StrDup((n * 17) + 7, "-"))
     End Function
 
-    'time = distance/velocity rearranged from velocity = (distance/time), assuming SI units.
+    ' Time = distance/velocity rearranged from velocity = (distance/time), assuming SI units.
     Function Time_Taken(distance As Double, speed As Integer)
-        t = (distance / speed)
+        Time_Taken = (distance / speed)
     End Function
 
-    'writing data to a text file on consecutive lines.
-    Function Write_To_txt(file_name, Data)
+    ' Writing data to a text file on consecutive lines.
+    Function Write_To_txt(file_path, Data)
+        Dim file_writer = My.Computer.FileSystem.OpenTextFileWriter(file_path, True)
 
-        'Creating a directory for the text file (optional, could be useful for keeping track of multiple text files)
-        My.Computer.FileSystem.CreateDirectory("Q1")
+        file_writer.WriteLine(Data)
 
-        'Creating/writing to a text file, located within the directory we just created above
-        My.Computer.FileSystem.WriteAllText("Q1\" & file_name, Data & vbNewLine, True)
-
+        file_writer.Close()
     End Function
 
-    'Reading data from particular lines in a text file.
-    Function Read_From_txt(file_name, line)
-        Dim file_reader = My.Computer.FileSystem.OpenTextFileReader("Q1\" & file_name)
+    ' Reading data from particular lines in a text file.
+    Function Read_From_txt(file_path, line)
+        Dim file_reader = My.Computer.FileSystem.OpenTextFileReader(file_path)
         Dim i = 0
 
         current_line = file_reader.ReadLine()
@@ -80,7 +73,7 @@ Module Module1
 
     End Function
 
-    'Average calculator.
+    ' Average calculator.
     Function Average(Total, number_of_data_samples)
         Average = Total / number_of_data_samples
     End Function
@@ -89,9 +82,9 @@ Module Module1
         Dim material = {"Wood    ", "Steel   ", "Titanium"}
         Dim Average_speed = {323.5, 180.8, 100.3}
         Dim depth(2)
-        Dim pre_average_time As Double 'the variable that will be used to calculate the average time.
+        Dim pre_average_time, t As Double 'pre_average_time that will be used to calculate the average time.
 
-        'q1 part c - fore user to input positive doubles. 
+        ' Q1 part c - fore user to input positive doubles. 
         For i = 0 To 2
             Console.WriteLine("Please enter the depth of " & Trim(material(i)) & " you are testing on in meters:")
             depth(i) = Console.ReadLine
@@ -107,38 +100,38 @@ Module Module1
 
         newlines(1)
 
-        'q1 part a.
-        '-part 1 - confirming users inputed depths with them.
+        ' Q1 part a.
+        ' -Part 1 - confirming users inputed depths with them.
         For i = 0 To 2
             Console.WriteLine("For distance in " & Trim(material(i)) & " you entered: " & depth(i) & " metres")
         Next
 
         newlines(1)
-        '-part 2 - forming a table for the time taken by the bullet to reach the target.
+        ' -Part 2 - forming a table for the time taken by the bullet to reach the target.
 
-        '--Designing  the table columns.
+        ' --Designing  the table columns.
         Table_Heading(2, "Material", "Time Taken (s)", "", "", "")
 
-        '--Designing  the table rows.
+        ' --Designing  the table rows.
         For i = 0 To 2
             Console.Write(material(i) & vbTab & "|" & vbTab)
-            Time_Taken(depth(i), Average_speed(i))
+            t = Time_Taken(depth(i), Average_speed(i))
             Console.WriteLine(Format(t, "0.000") & vbTab & vbTab & "|")
 
-            'storing the data in a text file for later use.
-            Write_To_txt("Bullet period in medium.txt", t)
+            '  Storing the data in a text file for later use.
+            Write_To_txt("C:\Users\Nickq20\Documents\UNI\Programming\Course_Work\Q1-Code\Data File.txt", t)
         Next
 
         newlines(1)
 
-        'q1 part b.
-        '-adding up pre stored times for each material so we can find the average.
+        ' Q1 part b.
+        ' -Adding up pre stored times for each material so we can find the average.
         For i = 1 To 3
-            Read_From_txt("Bullet period in medium.txt", i)
+            Read_From_txt("C:\Users\Nickq20\Documents\UNI\Programming\Course_Work\Q1-Code\Data File.txt", i)
             pre_average_time = current_line + pre_average_time
         Next
 
-        '-average calculation.
+        ' -Average calculation.
         Console.WriteLine("""The time taken by the bullet to reach the target on average"" = " & Format((Average(pre_average_time, 3)), "0.0000"))
     End Sub
 
