5b01693270e2d2ebaa9a37978a2a52e85959302c
Limesterlambo <liamgarmstrong1@aol.com>
2018-12-14 19:54:58 +0000

    Readability and Spelling/Grammar Corrections

--- q4.vb
+++ q4.vb
@@ -1,6 +1,6 @@
 Module Module1
-    'This programme has been designed to allow the scienctist to see how the different gunpowders affect the flight of the bullet being shot.'
-    'K E Hutton, December 2018'
+    'This program has been designed to allow the scientist to see how the different types of gunpowder affect the flight of the bullet being fired.
+    'K E Hutton, December 2018
 
     Dim distance
     Dim angle(999) As Double
@@ -15,7 +15,8 @@ Module Module1
     Dim height_of_target
     Dim Velocity_material_specific = {323.5, 180.8, 100.3}
 
-    'This fuction allows the user to see how random angles between the given minimum and maxium affect the time, accuracy and percentage. They can input their needed distance, velocity, material, gunpowder type to which this function will do the calculations.'
+    'This function allows the user to see how random angles between the given minimum and maximum affect the time, accuracy and percentage.
+    'They can input their required distance, velocity, material And gunpowder type for which this function will perform the calculations.
     Function Probability_of_Hits(angle_of_shot As Integer, height_of_target As Integer, distance As Integer, velocity As Double, gunpowder As String, Material As String)
 
         For i = 0 To 999
@@ -43,13 +44,14 @@ Module Module1
         average_time = total_time / count
         percentage_of_bullets = count / 1000 * 100
 
-        Console.WriteLine(total_time & " is the total time of all the bulltes fired from the gun in seconds.")
+        Console.WriteLine(total_time & " is the total time of all the bullets fired from the gun in seconds.")
         Console.WriteLine(count & " shots fired were below 100 meters out of the 1000 fired.")
         Console.WriteLine(average_time & " is the average time it took for the bullet to hit the target.")
-        Console.WriteLine(percentage_of_bullets & "% of bullets that hit the target for the " & gunpowder & " Gunpowder shooting at " & Material & ".")
+        Console.WriteLine(percentage_of_bullets & "% of bullets that hit the target for the " & gunpowder & " gunpowder shooting at " & Material & ".")
 
     End Function
-    'This fuction creates the table for the results.'
+
+    'This function creates the table for the results.
     Function Table_Heading(Number_of_columnns As Integer, Heading0 As String, Heading1 As String, Heading2 As String, Heading3 As String, Heading4 As String, Heading5 As String, Heading6 As String, Heading7 As String)
 
         Dim Headings(7) As String
@@ -70,11 +72,12 @@ Module Module1
         Next
 
 
-        ' If the table devision is off, modify the number of times "-" string is duplicated. 
+        'If the table division is off, modify the number of times "-" string is repeated. 
         Console.WriteLine(vbNewLine & StrDup((Number_of_columnns * 17) + 40, "-"))
 
     End Function
-    'This fuction allows the user to input different materials, distances, gunpowders and velocities and will output the accuracy and average time taken.'
+
+    'This function allows the user to input different materials, distances, gunpowders and velocities and will output the accuracy and average time taken.
     Function Accuracy_Table(angle_of_shot As Integer, height_of_target As Integer, distance As Integer, velocity As Double, material As String)
 
         For i = 0 To 999
@@ -112,7 +115,7 @@ Module Module1
     End Function
     Sub Main()
 
-        'Values inputed to calculate the total time, average and percentage for the brown gunpowder.'
+        'Values inputted to calculate the total time, average and percentage for the brown gunpowder.
         Probability_of_Hits(15, 100, 800, 323.5, "Brown", "Wood")
         Console.ReadLine()
         Probability_of_Hits(15, 100, 500, 180.8, "Brown", "Steel")
@@ -120,7 +123,7 @@ Module Module1
         Probability_of_Hits(15, 100, 250, 100.3, "Brown", "Titanium")
         Console.ReadLine()
 
-        'Values inputed to calculate the total time, average and percentage for the brown gunpowder.'
+        'Values inputted to calculate the total time, average and percentage for the brown gunpowder.
         Probability_of_Hits(10, 100, 800, 323.5, "Sulphur-free", "Wood")
         Console.ReadLine()
         Probability_of_Hits(10, 100, 500, 180.8, "Sulphur-free", "Steel")
@@ -128,7 +131,7 @@ Module Module1
         Probability_of_Hits(10, 100, 250, 100.3, "Sulphur-free", "Titanium")
         Console.ReadLine()
 
-        'Brown gunpowder shooting at wood with various distances.'
+        'Brown gunpowder shooting at wood with various distances.
         Console.WriteLine("Brown gunpowder being tested at various distances shooting at Wood.")
         Console.WriteLine("Material ¦ Distance (Meters) ¦ Acurracy (As Percentage) ¦ Time (Seconds)")
         Accuracy_Table(15, 100, 900, 323.5, "Wood")
@@ -136,7 +139,7 @@ Module Module1
         Accuracy_Table(15, 100, 100, 323.5, "Wood")
         Console.ReadLine()
 
-        'Brown gunpowder shooting at steel with various distances.'
+        'Brown gunpowder shooting at steel with various distances.
         Console.WriteLine("Brown gunpowder being tested at various distances shooting at Steel.")
         Console.WriteLine("Material ¦ Distance (Meters) ¦ Acurracy (As Percentage) ¦ Time (Seconds)")
         Accuracy_Table(15, 100, 900, 180.8, "Steel")
@@ -144,7 +147,7 @@ Module Module1
         Accuracy_Table(15, 100, 100, 180.8, "Steel")
         Console.ReadLine()
 
-        'Brown gunpowder shooting at titanium with various distances.'
+        'Brown gunpowder shooting at titanium with various distances.
         Console.WriteLine("Brown gunpowder being tested at various distances shooting at Titanium.")
         Console.WriteLine("Material ¦ Distance (Meters) ¦ Acurracy (As Percentage) ¦ Time (Seconds)")
         Accuracy_Table(15, 100, 900, 100.3, "Titanium")
@@ -152,7 +155,7 @@ Module Module1
         Accuracy_Table(15, 100, 100, 100.3, "Titanium")
         Console.ReadLine()
 
-        'Sulphur-free gunpowder shooting at wood with various distances.'
+        'Sulphur-free gunpowder shooting at wood with various distances.
         Console.WriteLine("Sulphur-free gunpowder being tested at various distances shooting at Wood.")
         Console.WriteLine("Material ¦ Distance (Meters) ¦ Acurracy (As Percentage) ¦ Time (Seconds)")
         Accuracy_Table(10, 100, 900, 323.5, "Wood")
@@ -160,7 +163,7 @@ Module Module1
         Accuracy_Table(10, 100, 100, 323.5, "Wood")
         Console.ReadLine()
 
-        'Sulphur-free gunpowder shooting at steel with various distances.'
+        'Sulphur-free gunpowder shooting at steel with various distances.
         Console.WriteLine("Sulphur-free gunpowder being tested at various distances shooting at Steel.")
         Console.WriteLine("Material ¦ Distance (Meters) ¦ Acurracy (As Percentage) ¦ Time (Seconds)")
         Accuracy_Table(10, 100, 900, 180.8, "Steel")
@@ -168,7 +171,7 @@ Module Module1
         Accuracy_Table(10, 100, 100, 180.8, "Steel")
         Console.ReadLine()
 
-        'Sulphur-free gunpowder shooting at titanium with various distances.'
+        'Sulphur-free gunpowder shooting at titanium with various distances.
         Console.WriteLine("Sulphur-free gunpowder being tested at various distances shooting at Titanium.")
         Console.WriteLine("Material ¦ Distance (Meters) ¦ Acurracy (As Percentage) ¦ Time (Seconds)")
         Accuracy_Table(10, 100, 900, 100.3, "Titanium")
@@ -176,9 +179,10 @@ Module Module1
         Accuracy_Table(10, 100, 100, 100.3, "Titanium")
         Console.ReadLine()
 
-        'Results gained from the tests completed above.'
-        Console.WriteLine("From the tests completed above this shows that if the distance from the gun to the target is increased the accuracy and the time taken increases also.")
-        Console.WriteLine("With use of different materials they all gave different results, from the tests done it shows that when using the wood it has the smallest avergae time but is the least accurate out of the three materials. Furthermore the titanium took the longest average time but turned out to be the most accurate and the steel feel in the middle of the wood and the titanium.")
+        'Results gained from the tests completed above.
+        Console.WriteLine("From the tests completed above this shows that if the distance from the gun to the target is increased, the accuracy and the time taken increases also.")
+        Console.WriteLine("With use of different materials they all gave different results, from the tests done it shows that when using the wood it has the smallest average time but is the least accurate " _
+                          & "out of the three materials. Furthermore, the titanium took the longest average time but turned out to be the most accurate and the steel fell between the wood and the titanium.")
         Console.ReadLine()
 
     End Sub
