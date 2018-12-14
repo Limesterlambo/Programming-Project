3f5410ac8b0d6353c36bd8133f8a6b6836676dfc
SarahJoyBrittain <45204786+SarahJoyBrittain@users.noreply.github.com>
2018-12-14 22:22:49 +0000

    Add files via upload

--- q2.vb
+++ q2.vb
@@ -1,7 +1,15 @@
-﻿Module Module1
-    Dim t
+﻿' Program Asks user to imput depths for each material. Prgram Calculates the expected average time taken for six differnt combinations of modifications.
+' Programs prints out a table showing the expected time taken for three materials and their average for each of the combination.
+' Program then organises the average time taken overall for each modification as well as no modifications from longest to shortes and prints the information
+' into a text file.
+' Authers: Liam Armstrong, Sarah Joy Brittain, December 2018
 
-    '  Function To seperate work out in the console.
+Module Module1
+    ' Formats and prints arguments for table rows. Space specialised for material and time taken.
+    Function Table_Row(x, y)
+        Console.WriteLine(Space(2) & x & vbTab & "|" & Space(2) & y & vbTab & vbTab & "|")
+    End Function
+    '  Prints out new lines in the console.
     Function Newlines(Number_of_newlines As Integer)
         For i = 1 To Number_of_newlines
             Console.Write(vbNewLine)
@@ -17,7 +25,7 @@
         Headings(3) = Heading3
         Headings(4) = Heading4
 
-        ' If the table division is off, modify the number of times "-" string is repeated.
+        ' Table division: If the table division is off, modify the number of times "-" string is repeated.
         Console.WriteLine(StrDup((Number_of_columnns * 17) + 7, "="))
         For I = 0 To 4
             If Number_of_columnns > I Then
@@ -40,29 +48,17 @@
     End Function
 
     Sub Main()
+
         Dim material = {"Wood    ", "Steel   ", "Titanium"}
-        Dim Average_speed = {323.5, 180.8, 100.3}
-        Dim Average_Time_taken(6) As Double
-
-        Dim depth(2)
-        Dim j
-        Dim Total_time_Combination As Double
-        Dim Total_Time
-
-        'Array to contain, sorted from smallest to largest, average time taken(s) of materials in each combination.
-        Dim Sorted_Average_Time_Taken(6) As Double
-        ' Creating an array containing all possible combination titles that will be needed for the table.
+        ' Creating an array containing all possible combination titles that will be needed e.g. for the table. 
+        ' The tilte's corresponding element number used here are used as the numbers to reference the combinations within other arrays.
         Dim Combination_Titles() As String = {"Blanks and brown gunpowder" & Space(15), "Lead and brown gunpowder" & Space(20), "Armor-piercing and brown gunpowder" & Space(10),
             "Blanks and sulphur-free gunpowder" & Space(8), "Lead and sulphur-free gunpowder" & Space(10), "Armor-piercing and sulphur-free gunpowder", "No modifications" & Space(25)}
 
-        ' Creating three arrays, one for each material, to store the modified setup's average speeds. This array will be be used to calculate the times taken.
-        Dim Mod_Average_Speed_Wood(2, 1), Mod_Average_Speed_Steel(2, 1), Mod_Average_Speed_Titanium(2, 1) As Double
+        Dim depth(2)
+        Dim Is_It_Numerical As Boolean ' Test part.
 
-        'Creating arrays for time taken in each material.
-        Dim Time_Taken_Wood(6), Time_Taken_Steel(6), Time_Taken_Titanium(6) As Double
-
-        Dim l As Integer
-
+        Dim Average_speed = {323.5, 180.8, 100.3}
         ' Array Bullet() elements correspond To 0 blank, 1 LeadBullets, 2 Armour-piercing Bullets.
         ' Array Gunpowder() elements correspond to 0 Brown Gunpowder, 1 Sulphur-free Gunowder.
         Dim Wood_Bullets() As Integer = {-3, 20, -30}
@@ -72,6 +68,19 @@
         Dim Titanium_Bullets() As Integer = {-12, 5, 30}
         Dim Titanium_Gunpowder() As Integer = {35, -10}
 
+        ' Creating three arrays, one for each material, to store the modified setup's average speeds. This array will be be used to calculate the times taken.
+        Dim Mod_Average_Speed_Wood(2, 1), Mod_Average_Speed_Steel(2, 1), Mod_Average_Speed_Titanium(2, 1) As Double
+        ' Creating arrays for time taken in each material.
+
+        Dim Count As Integer ' Calculation part.
+        Dim Time_Taken_Wood(6), Time_Taken_Steel(6), Time_Taken_Titanium(6) As Double
+
+        Dim Total_time_Combination As Double ' Calculation part.
+        Dim Average_Time_taken(6) As Double
+
+        ' Array to containg, sorted from smallest to largest, average time taken across the three materials in each combination.
+        Dim Sorted_Average_Time_Taken(6) As Double
+
 
         ' Q1 part (c) - forcing the user to input positive doubles. 
         For i = 0 To 2
@@ -79,12 +88,12 @@
             depth(i) = Console.ReadLine
 
             ' Loop Making sure the assigned value is a number.
-            Int32.TryParse(depth(i), j)
-            While j = False
+            Int32.TryParse(depth(i), Is_It_Numerical)
+            While Is_It_Numerical = False
                 Console.WriteLine("ERROR: depth must be a numerical! " &
                     "Please re-enter the depth of " & LCase(Trim(material(i))) & " you are testing on in meters:")
                 depth(i) = Console.ReadLine()
-                Int32.TryParse(depth(i), j)
+                Int32.TryParse(depth(i), Is_It_Numerical)
             End While
             depth(i) = CDbl(depth(i))
 
@@ -107,29 +116,31 @@
         Next
 
 
+        ' Calculating the Time Taken for No modifications
         Time_Taken_Wood(6) = Calculate_Time_Taken(depth(0), Average_speed(0))
         Time_Taken_Steel(6) = Calculate_Time_Taken(depth(1), Average_speed(1))
         Time_Taken_Titanium(6) = Calculate_Time_Taken(depth(2), Average_speed(2))
         Total_time_Combination = Time_Taken_Wood(6) + Time_Taken_Steel(6) + Time_Taken_Titanium(6)
         Average_Time_taken(6) = Average(Total_time_Combination, 3)
 
-        ' Loop with nested Loop used to enter the variables for each combination of setup into the calculation of a new average time to be assigned into the array.
-        For X = 0 To 1
-            For I = 0 To 2
+        ' Loop with nested Loop used to assign and calculate average speeds for each Modification combination.
+        For G = 0 To 1
+            For B = 0 To 2
                 ' Calculates the new average time taken by the effect of modifications, Mod_Average_Speed Array's fist row = Bullet used and Second row = Gunpowder used.
-                Mod_Average_Speed_Wood(I, X) = Average_speed(0) * (1 + (Wood_Bullets(I) + Wood_Gunpowder(X)) / 100)
-                Mod_Average_Speed_Steel(I, X) = Average_speed(1) * (1 + (Steel_Bullets(I) + Steel_Gunpowder(X)) / 100)
-                Mod_Average_Speed_Titanium(I, X) = Average_speed(2) * (1 + (Titanium_Bullets(I) + Titanium_Gunpowder(X)) / 100)
+                Mod_Average_Speed_Wood(B, G) = Average_speed(0) * (1 + (Wood_Bullets(B) + Wood_Gunpowder(G)) / 100)
+                Mod_Average_Speed_Steel(B, G) = Average_speed(1) * (1 + (Steel_Bullets(B) + Steel_Gunpowder(G)) / 100)
+                Mod_Average_Speed_Titanium(B, G) = Average_speed(2) * (1 + (Titanium_Bullets(B) + Titanium_Gunpowder(G)) / 100)
             Next
         Next
 
-        l = 0
-        For X = 0 To 1
-            For I = 0 To 2
-                Time_Taken_Wood(l) = Calculate_Time_Taken(depth(0), Mod_Average_Speed_Wood(I, X))
-                Time_Taken_Steel(l) = Calculate_Time_Taken(depth(1), Mod_Average_Speed_Steel(I, X))
-                Time_Taken_Titanium(l) = Calculate_Time_Taken(depth(2), Mod_Average_Speed_Titanium(I, X))
-                l = l + 1
+        ' Loop with nested loop and count for each combination of modification, used to assign and calculate time taken for each material.
+        Count = 0
+        For G = 0 To 1
+            For B = 0 To 2
+                Time_Taken_Wood(Count) = Calculate_Time_Taken(depth(0), Mod_Average_Speed_Wood(B, G))
+                Time_Taken_Steel(Count) = Calculate_Time_Taken(depth(1), Mod_Average_Speed_Steel(B, G))
+                Time_Taken_Titanium(Count) = Calculate_Time_Taken(depth(2), Mod_Average_Speed_Titanium(B, G))
+                Count = Count + 1
             Next
         Next
 
@@ -140,12 +151,9 @@
             Console.WriteLine(Space(1) & UCase(Trim(Combination_Titles(I))) & ":")
 
             Table_Heading(2, "Material", "Time taken (s)", "", "", "")  'Console.Write(Space(2)) at front 
-            Console.WriteLine(Space(2) & material(0) & vbTab &
-                              "|" & Space(2) & Format(Time_Taken_Wood(I), "0.000") & vbTab & vbTab & "|")
-            Console.WriteLine(Space(2) & material(1) & vbTab &
-                              "|" & Space(2) & Format(Time_Taken_Steel(I), "0.000") & vbTab & vbTab & "|")
-            Console.WriteLine(Space(2) & material(2) & vbTab &
-                              "|" & Space(2) & Format(Time_Taken_Titanium(I), "0.000") & vbTab & vbTab & "|")
+            Table_Row(material(0), Format(Time_Taken_Wood(I), "0.000"))
+            Table_Row(material(1), Format(Time_Taken_Steel(I), "0.000"))
+            Table_Row(material(2), Format(Time_Taken_Titanium(I), "0.000"))
             Console.WriteLine(StrDup((2 * 17) + 7, "="))
 
             Total_time_Combination = Time_Taken_Wood(I) + Time_Taken_Steel(I) + Time_Taken_Titanium(I)
@@ -161,9 +169,9 @@
 
         ' Creating the directory and file for the data and writing the headings to the file.
         My.Computer.FileSystem.CreateDirectory("ExperimentalData")
-        My.Computer.FileSystem.WriteAllText("ExperimentalData\Data.txt", "=========================================================================" & vbNewLine _
+        My.Computer.FileSystem.WriteAllText("ExperimentalData\Data.txt", StrDup(73, "=") & vbNewLine _
                                             & "  Modifications" & vbTab & vbTab & vbTab & vbTab & vbTab & "|" & "  Average time taken (s)" & vbNewLine _
-                                            & "=========================================================================" & vbNewLine, False)
+                                            & StrDup(73, "=") & vbNewLine, False)
 
         ' Writing the data to the file.
         For I = 0 To 6
